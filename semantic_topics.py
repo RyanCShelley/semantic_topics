6ed96483b7e1beeba61495363d3ccb8a69fa4e41
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-02-23 07:32:00 -0500

    Update semantic_topics.py

--- semantic_topics.py
+++ semantic_topics.py
@@ -13,43 +13,42 @@ from wordcloud import WordCloud
 
 site_url = st.text_input('URL to Crawl', '')
 
-if site_url is not None:
-
-	crawl(site_url, 'site_crawl.jl', follow_links=True, custom_settings={'CLOSESPIDER_PAGECOUNT': 3000})
-	df = pd.read_json('site_crawl.jl', lines=True)
-	df.head()
-
-	urls = df['url'].values.tolist()
-
-	df_url = pd.DataFrame(urls,columns =['urls'])
-
-	result = []
 
 
-	# Loop items in results
-	for page in df_url['urls']:
-		downloaded = trafilatura.fetch_url(page)
-		if downloaded is not None: # assuming the download was successful
-			result.append(trafilatura.extract(downloaded, include_tables=False, include_formatting=False, include_comments=False))
-		else:
-			result.append("No Text")
+crawl(site_url, 'site_crawl.jl', follow_links=True, custom_settings={'CLOSESPIDER_PAGECOUNT': 3000})
+df = pd.read_json('site_crawl.jl', lines=True)
 
-	df_url["result"] = result
+st.dataframe(df)
 
+urls = df['url'].values.tolist()
 
-	#cleaning the text
+df_url = pd.DataFrame(urls,columns =['urls'])
 
-	df_url['result'] = df_url['result'].astype(str)
-	result = ','.join(list(df_url['result'].values))
-	result = result.translate(result.maketrans('', '', string.punctuation))
-	result = result.lower()
+result = []
 
 
-	# Create a WordCloud object
-	wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue', width=800, height=400)
-	#Generate a word cloud
-	wordcloud.generate(result)
-	# Visualize the word cloud
-	wordcloud.to_image()
+# Loop items in results
+for page in df_url['urls']:
+	downloaded = trafilatura.fetch_url(page)
+	if downloaded is not None: # assuming the download was successful
+		result.append(trafilatura.extract(downloaded, include_tables=False, include_formatting=False, include_comments=False))
+	else:
+		result.append("No Text")
+
+df_url["result"] = result
 
 
+#cleaning the text
+
+df_url['result'] = df_url['result'].astype(str)
+result = ','.join(list(df_url['result'].values))
+result = result.translate(result.maketrans('', '', string.punctuation))
+result = result.lower()
+
+
+# Create a WordCloud object
+wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue', width=800, height=400)
+#Generate a word cloud
+wordcloud.generate(result)
+# Visualize the word cloud
+wordcloud.to_image()
