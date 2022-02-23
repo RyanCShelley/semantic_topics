f0a82058b9d2640a54709477753bfe28c8861288
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-02-23 07:41:15 -0500

    Update semantic_topics.py

--- semantic_topics.py
+++ semantic_topics.py
@@ -1,54 +1,100 @@
 import streamlit as st
 
 import pandas as pd
-import advertools as adv
-from advertools import crawl
-import logging
-from time import time
-from collections import Counter
+import requests
+import urllib
+import pandas as pd
+from requests_html import HTML
+from requests_html import HTMLSession
 import trafilatura
 import string
 from wordcloud import WordCloud
 
 
-site_url = st.text_input('URL to Crawl', '')
+#Scraping Title, Link and Text
+
+def get_results(query):
+    
+    query = urllib.parse.quote_plus(query)
+    response = get_source("https://www.google.com/search?q=" + query)
+    
+    return response
 
 
+def parse_results(response):
+    
+    css_identifier_result = ".tF2Cxc"
+    css_identifier_title = "h3"
+    css_identifier_link = ".yuRUbf a"
+    css_identifier_text = ".IsZvec"
+    
+    results = response.html.find(css_identifier_result)
 
-crawl(site_url, 'site_crawl.jl', follow_links=True, custom_settings={'CLOSESPIDER_PAGECOUNT': 3000})
-df = pd.read_json('site_crawl.jl', lines=True)
+    output = []
+    
+    for result in results:
 
-st.dataframe(df)
+        item = {
+            'title': result.find(css_identifier_title, first=True).text,
+            'link': result.find(css_identifier_link, first=True).attrs['href'],
+        }
+        
+        output.append(item)
+        
+    return output
 
-urls = df['url'].values.tolist()
+def google_search(query):
+    response = get_results(query)
+    return parse_results(response)
 
-df_url = pd.DataFrame(urls,columns =['urls'])
 
-result = []
+def get_source(url):
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        return response 
+    except requests.exceptions.RequestException as e:
+        print(e)    
 
+
+query = st.text_input('Query', '')
+
+results = google_search(query)
+
+#Your Indexed Pages
+
+
+df = pd.DataFrame(results)
+
+content = []
 
 # Loop items in results
-for page in df_url['urls']:
-	downloaded = trafilatura.fetch_url(page)
-	if downloaded is not None: # assuming the download was successful
-		result.append(trafilatura.extract(downloaded, include_tables=False, include_formatting=False, include_comments=False))
-	else:
-		result.append("No Text")
+for page in df['link']:
+  downloaded = trafilatura.fetch_url(page)
+  if downloaded is not None: # assuming the download was successful
+    content.append(trafilatura.extract(downloaded, include_tables=False, include_formatting=False, include_comments=False))
+  else:
+    content.append("No Text")
 
-df_url["result"] = result
+df["content"] = content
 
 
-#cleaning the text
+#Analysing the SERPs
 
-df_url['result'] = df_url['result'].astype(str)
-result = ','.join(list(df_url['result'].values))
-result = result.translate(result.maketrans('', '', string.punctuation))
-result = result.lower()
+df['content'] = df['content'].astype(str)
 
+text = ','.join(list(df['content'].values))
+text = text.translate(text.maketrans('', '', string.punctuation))
+text = text.lower()
+
+#Word Cloud
+
+# Import the wordcloud library
+from wordcloud import WordCloud
 
 # Create a WordCloud object
 wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue', width=800, height=400)
-#Generate a word cloud
-wordcloud.generate(result)
+# Generate a word cloud
+wordcloud.generate(text)
 # Visualize the word cloud
 wordcloud.to_image()
